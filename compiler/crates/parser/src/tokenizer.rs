f611aab1f077fff3cb38e3ab7bce4fbcf9bb4a1f
hydroper <matheusdiasdesouzads@gmail.com>
2023-10-17 07:54:10 -0300

    _

--- compiler/crates/parser/src/tokenizer.rs
+++ compiler/crates/parser/src/tokenizer.rs
@@ -1,5 +1,5 @@
 use std::rc::Rc;
-use crate::{Source, util::CodePointsReader, IntolerableError};
+use crate::{Source, util::CodePointsReader, IntolerableError, Location, character_validation};
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum Token {
@@ -24,10 +24,31 @@ impl<'input> Tokenizer<'input> {
         }
     }
 
-    pub fn next(&mut self, reserved_words: bool) -> Result<Token, IntolerableError> {
+    pub fn next(&mut self, reserved_words: bool) -> Result<(Token, Location), IntolerableError> {
         loop {
             let ch = self.code_points.peek_or_zero();
+            if character_validation::is_whitespace(ch) {
+                self.code_points.next();
+            } else if !(self.consume_line_terminator() || self.consume_comment()) {
+                break;
+            }
         }
+        let start = self.current_character_location();
         final_result_here
+    }
+
+    pub fn current_line_number(&self) -> usize {
+        self.current_line_number
+    }
+
+    pub fn current_character_location(&self) -> Location {
+        let offset = self.code_points.index();
+        Location {
+            source: Rc::clone(&self.source),
+            first_line_number: self.current_line_number,
+            last_line_number: self.current_line_number,
+            first_offset: offset,
+            last_offset: offset,
+        }
     }
 }
