7c36b766d7f36d3dc41779aef44453493b0d244e
hydroper <matheusdiasdesouzads@gmail.com>
2024-04-26 19:58:32 -0300

    Error enhancing

--- crates/parser/parser/css_parser.rs
+++ crates/parser/parser/css_parser.rs
@@ -64,6 +64,9 @@ impl<'input> CssParser<'input> {
 
     /*
     fn add_warning(&self, location: &Location, kind: DiagnosticKind, arguments: Vec<DiagnosticArgument>) {
+        if self.compilation_unit().prevent_equal_offset_warning(location) {
+            return;
+        }
         self.compilation_unit().add_diagnostic(Diagnostic::new_warning(location, kind, arguments));
     }
     */
